9a6c2c849fb90da4eb61d0b52a896dfe89cb9c92
benjaminpeto <benjaminpeto23@gmail.com>
2025-02-20 09:37:44 +0000

    move out geo locations to their own file

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,5 +1,7 @@
 "use client";
+
 import { WorldMap } from "@/components/ui/world-map";
+import { countryGeoLocation } from "@/misc/countries-geo-location";
 import { motion } from "framer-motion";
 
 export function WorldMapDemo() {
@@ -7,9 +9,9 @@ export function WorldMapDemo() {
     <div className="dark:bg-black bg-white w-full py-16">
       <div className="max-w-7xl mx-auto text-center">
         <h1 className="font-bold text-xl md:text-4xl dark:text-white text-black">
-          Talent{" "}
+          Secret Source&apos;s{" "}
           <span className="text-neutral-400">
-            {"Pool".split("").map((word, idx) => (
+            {"Talent Pool".split("").map((word, idx) => (
               <motion.span
                 key={idx}
                 className="inline-block"
@@ -26,34 +28,7 @@ export function WorldMapDemo() {
           We are connecting top talents with the best companies around Europe.
         </p>
       </div>
-      <WorldMap
-        dots={[
-          {
-            start: { lat: 17.2916, lng: -16.6291 }, // Canary Islands
-            end: { lat: 42.5074, lng: -0.1278 }, // London
-          },
-          {
-            start: { lat: 42.5074, lng: -0.1278 }, // London
-            end: { lat: 45.4501, lng: 30.5234 }, // Kiev
-          },
-          {
-            start: { lat: 17.2916, lng: -16.6291 }, // Canary Islands
-            end: { lat: 28.4168, lng: -3.7038 }, // Madrid
-          },
-          {
-            start: { lat: 17.2916, lng: -16.6291 }, // Canary Islands
-            end: { lat: 38.4979, lng: 19.0402 }, // Budapest
-          },
-          {
-            start: { lat: 17.2916, lng: -16.6291 }, // Canary Islands
-            end: { lat: 41.3676, lng: 4.9041 }, // Amsterdam
-          },
-          {
-            start: { lat: 36.8781, lng: -87.6298 }, // Chicago
-            end: { lat: 17.2916, lng: -16.6291 }, // Canary Islands
-          },
-        ]}
-      />
+      <WorldMap dots={countryGeoLocation} />
     </div>
   );
 }
---
+++ src/misc/countries-geo-location.ts
@@ -0,0 +1,26 @@
+export const countryGeoLocation = [
+  {
+    start: { lat: 17.2916, lng: -16.6291 }, // Canary Islands
+    end: { lat: 42.5074, lng: -0.1278 }, // London
+  },
+  {
+    start: { lat: 42.5074, lng: -0.1278 }, // London
+    end: { lat: 45.4501, lng: 30.5234 }, // Kiev
+  },
+  {
+    start: { lat: 17.2916, lng: -16.6291 }, // Canary Islands
+    end: { lat: 28.4168, lng: -3.7038 }, // Madrid
+  },
+  {
+    start: { lat: 17.2916, lng: -16.6291 }, // Canary Islands
+    end: { lat: 38.4979, lng: 19.0402 }, // Budapest
+  },
+  {
+    start: { lat: 17.2916, lng: -16.6291 }, // Canary Islands
+    end: { lat: 41.3676, lng: 4.9041 }, // Amsterdam
+  },
+  {
+    start: { lat: 36.8781, lng: -87.6298 }, // Chicago
+    end: { lat: 17.2916, lng: -16.6291 }, // Canary Islands
+  },
+];
